d9042c37de10f13bc842145ccc67fa11e938bb7e
Azuki Yamada <yamada.azuki@aoaoi.work>
2021-11-06 00:55:51 +0900

    見かけだけの棒グラフ

--- src/components/GitHub.jsx
+++ src/components/GitHub.jsx
@@ -11,6 +11,72 @@ const GitHub = () => {
                 <p>Contributions</p>
                 <hr/>
             </div>
+            <ul className="graph">
+                <li>
+                    <div className="bar bar1"></div>
+                    <p>Jul</p>
+                </li>
+                <li>
+                    <div className="bar bar2"></div>
+                    <p>　　</p>
+                </li>
+                <li>
+                    <div className="bar bar3"></div>
+                    <p>　　</p>
+                </li>
+                <li>
+                    <div className="bar bar4"></div>
+                    <p>　　</p>
+                </li>
+                <li>
+                    <div className="bar bar5"></div>
+                    <p>Aug</p>
+                </li>
+                <li>
+                    <div className="bar bar6"></div>
+                    <p>　　</p>
+                </li>
+                <li>
+                    <div className="bar bar7"></div>
+                    <p>　　</p>
+                </li>
+                <li>
+                    <div className="bar bar8"></div>
+                    <p>　　</p>
+                </li>
+                <li>
+                    <div className="bar bar9"></div>
+                    <p>Sep</p>
+                </li>
+                <li>
+                    <div className="bar bar10"></div>
+                    <p>　　</p>
+                </li>
+                <li>
+                    <div className="bar bar11"></div>
+                    <p>　　</p>
+                </li>
+                <li>
+                    <div className="bar bar12"></div>
+                    <p>　　</p>
+                </li>
+                <li>
+                    <div className="bar bar13"></div>
+                    <p>Oct</p>
+                </li>
+                <li>
+                    <div className="bar bar14"></div>
+                    <p>　　</p>
+                </li>
+                <li>
+                    <div className="bar bar15"></div>
+                    <p>　　</p>
+                </li>
+                <li>
+                    <div className="bar bar16"></div>
+                    <p>　　</p>
+                </li>
+            </ul>
         </div>
     );
 }
